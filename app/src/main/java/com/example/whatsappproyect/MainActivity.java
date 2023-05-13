0d5a7054728f814d1900182a0d0811b6dfd3f4d5
mnuel <a20310057@ceti.mx>
2023-05-13 05:51:43 -0600

    Captura email y password de los Inputs

--- app/src/main/java/com/example/whatsappproyect/MainActivity.java
+++ app/src/main/java/com/example/whatsappproyect/MainActivity.java
@@ -4,17 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
     TextView mTextViewRegister;
+    TextInputEditText mtTextInputEmail;
+    TextInputEditText mtTextInputPassword;
+    Button btnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mTextViewRegister = findViewById(R.id.textViewRegister);
+        mtTextInputEmail = findViewById(R.id.textInputEmail);
+        mtTextInputPassword = findViewById(R.id.textInputPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+
         mTextViewRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -22,5 +41,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void login(){
+        String email = mtTextInputEmail.getText().toString();
+        String password = mtTextInputPassword.getText().toString();
+        Log.d("campo", "Email: " + email);
+        Log.d("campo", "Password: " + password);
     }
 }
