01779caaf43782d03cae95f90f77bf0333a91b60
mnuel <a20310057@ceti.mx>
2023-05-13 05:36:01 -0600

    Vista de Registro terminada

--- app/src/main/java/com/example/whatsappproyect/RegisterActivity.java
+++ app/src/main/java/com/example/whatsappproyect/RegisterActivity.java
@@ -3,12 +3,23 @@ package com.example.whatsappproyect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    CircleImageView mcircleImageViewBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mcircleImageViewBack = findViewById(R.id.circleImageBack);
+        mcircleImageViewBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
