8562deebe051c3065f26e7fcea8c49359a424153
mnuel <a20310057@ceti.mx>
2023-05-14 17:40:25 -0600

    Implementacipon de FirebaseAuthentication

--- app/src/main/java/com/example/whatsappproyect/RegisterActivity.java
+++ app/src/main/java/com/example/whatsappproyect/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.example.whatsappproyect;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,7 +8,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,6 +24,7 @@ public class RegisterActivity extends AppCompatActivity {
     CircleImageView mcircleImageViewBack;
     TextInputEditText mTextInputUsername, getmTextInputEmail, getmTextInputPassword,getmTextInputConfirmPassword;
     Button btnRegister;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +37,7 @@ public class RegisterActivity extends AppCompatActivity {
         getmTextInputPassword = findViewById(R.id.textInputPassword);
         getmTextInputConfirmPassword = findViewById(R.id.textInputPasswordConfirmar);
         btnRegister = findViewById(R.id.btnRegister);
+        mAuth = FirebaseAuth.getInstance();
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,18 +60,42 @@ public class RegisterActivity extends AppCompatActivity {
         String confirmpassword = getmTextInputConfirmPassword.getText().toString();
 
         if(!username.isEmpty() && !email.isEmpty() && !password.isEmpty() && !confirmpassword.isEmpty()){
-            if(isEmailValid(email))Toast.makeText(this, "Has ingresado todos los datos y el correno es valdio", Toast.LENGTH_LONG).show();
-            else Toast.makeText(this, "Has ingresado todos los datos pero el correno no es valdio :c", Toast.LENGTH_LONG).show();
+            if(isEmailValid(email)){
+                if(password.equals(confirmpassword)){
+                    if (password.length() >= 6){
+                        createUser(email,password);
+                    }else {
+                        Toast.makeText(this, "La contraseña debe tener al menos 6 caracteres", Toast.LENGTH_LONG).show();
+                    }
+                }
+                Toast.makeText(this, "Has ingresado todos los datos y el correno es valdio", Toast.LENGTH_LONG).show();
+            }
+            else{
+                Toast.makeText(this, "Has ingresado todos los datos pero el correno no es valdio :c", Toast.LENGTH_LONG).show();
+            }
         }else{
             Toast.makeText(this, "Ingresa todos los datos para continuar :c", Toast.LENGTH_LONG).show();
         }
     }
+
 
     public boolean isEmailValid(String email) {
         String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
         Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
-    }  
+    }
 
+   private void createUser (String email,String password){
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(RegisterActivity.this, "Usuario registrado :)", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(RegisterActivity.this, "No se pudo registrar el usuario :c", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+   }
 }
