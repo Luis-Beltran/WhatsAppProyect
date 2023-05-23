cabdbe35258f4c5c808b6d2c4ec7d1f422bcb1d0
mnuel <a20310057@ceti.mx>
2023-05-23 14:34:09 -0600

    Boton Para publicar

--- app/src/main/java/com/example/whatsappproyect/fragments/HomeFragment.java
+++ app/src/main/java/com/example/whatsappproyect/fragments/HomeFragment.java
@@ -1,5 +1,8 @@
 package com.example.whatsappproyect.fragments;
 
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,9 +12,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.whatsappproyect.R;
+import com.example.whatsappproyect.activities.PostActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Random;
 
 public class HomeFragment extends Fragment {
-
+    View mview;
+    FloatingActionButton mFab;
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -20,6 +28,21 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        mview = inflater.inflate(R.layout.fragment_home, container, false);
+        mFab = mview.findViewById(R.id.fab);
+
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToPost();
+            }
+        });
+        return mview;
     }
+
+    private void goToPost() {
+        Intent intent = new Intent(getContext(), PostActivity.class);
+        startActivity(intent);
+    }
+
 }
