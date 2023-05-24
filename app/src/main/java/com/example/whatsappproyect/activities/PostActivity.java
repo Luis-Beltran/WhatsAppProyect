7d177c84698484db3f260e322df9c557c672a599
mnuel <a20310057@ceti.mx>
2023-05-24 13:08:55 -0600

    Almacena imganes en StorageFireBase

--- app/src/main/java/com/example/whatsappproyect/activities/PostActivity.java
+++ app/src/main/java/com/example/whatsappproyect/activities/PostActivity.java
@@ -1,16 +1,113 @@
 package com.example.whatsappproyect.activities;
 
+import androidx.annotation.NonNull;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.UploadTask;
 
 import com.example.whatsappproyect.R;
+import com.example.whatsappproyect.providers.ImageProvider;
+import com.example.whatsappproyect.utils.FileUtil;
+
+import java.io.File;
 
 public class PostActivity extends AppCompatActivity {
-
+    ImageView mImageViewPost1;
+    private final int GALLERY_REQUEST_CODE = 1;
+    File mImageFile;
+    ImageProvider mImageProvider;
+    Button mButtonPost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        mImageProvider = new ImageProvider();
+
+        mImageViewPost1 = findViewById(R.id.imageViewPost1);
+        mButtonPost = findViewById(R.id.btnPost);
+
+        mButtonPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveImage();
+            }
+        });
+        mImageViewPost1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGallery();
+            }
+        });
     }
+
+    private void saveImage() {
+        mImageProvider.save(PostActivity.this, mImageFile).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(PostActivity.this, "La imagen se almaceno correctamente", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(PostActivity.this, "Hubo error al almacenar la imagen", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    ActivityResultLauncher<Intent> galleryLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == Activity.RESULT_OK){
+                        try {
+                            mImageFile = FileUtil.from(PostActivity.this,result.getData().getData());
+                            mImageViewPost1.setImageBitmap(BitmapFactory.decodeFile(mImageFile.getAbsolutePath()));
+                        }catch (Exception e){
+                            Log.d("ERROR", "Se produjo un error "+e.getMessage());
+                            Toast.makeText(PostActivity.this,"Se produjo un error "+e.getMessage(),Toast.LENGTH_SHORT);
+                        }
+                    }
+                }
+            }
+    );
+    private void openGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        galleryLauncher.launch(galleryIntent);
+        //startActivityForResult(galleryIntent,GALLERY_REQUEST_CODE);
+    }
+
+    /*@Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GALLERY_REQUEST_CODE && resultCode == RESULT_OK){
+            try {
+                mImageFile = FileUtil.from(this,data.getData());
+                mImageViewPost1.setImageBitmap(BitmapFactory.decodeFile(mImageFile.getAbsolutePath()));
+            }catch (Exception e){
+                Log.d("ERROR", "Se produjo un error "+e.getMessage());
+                Toast.makeText(this,"Se produjo un error "+e.getMessage(),Toast.LENGTH_SHORT);
+            }
+        }
+    }*/
 }
